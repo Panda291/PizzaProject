26752ccb0f876f3331c1e78fba50ebff63be239f
Panad <christophevandeneede@hotmail.com>
2019-11-07 10:52:04 +0100

    FAB added and onclick listener implemented

--- app/src/main/java/com/example/pizzaProject/MainActivity.java
+++ app/src/main/java/com/example/pizzaProject/MainActivity.java
@@ -9,6 +9,9 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
         {
             Log.d("Main/OnCreate", "tablet");
         }
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("FAB", "clicked");
+            }
+        });
     }
 }
